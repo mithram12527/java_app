fc13e2c738304627712817a36ef8412f6d727ff8
EC2 Default User <ec2-user@ip-172-31-32-205.ap-south-1.compute.internal>
2021-06-11 10:34:02 +0000

    added paste

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("good day");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
+    System.out.println("good day everyone");
   }
 }
